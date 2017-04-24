409eb1882af4ed80cb506044ed30b378a278a9d7
MrMurdoc <MrMurdoc@MrMurdoc.wifirouter.net>
2017-04-24 15:09:24 -0300

    Foram adicionados comentarios e algumas alterações essenciais

--- src/aplicação/Gerenciador.java
+++ src/aplicação/Gerenciador.java
@@ -1,29 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicação;
 
 /**
  *
  * @author MrMurdoc
+ * @author Elfa1
  */
 public class Gerenciador {
-    
+    /**
+     * Atributos da Classe Gerencidor 
+     */
     private Agenda Agenda;
     private Thread MainThread;
-    
+
     public Agenda lerDados(){
-        return null;
-        
+        return null;     
     }
     
     public void salvarDados(Agenda agenda){
         
     }
     
-    public static void aleta (Tarefa tarefa){
+    public static void alertar (Tarefa tarefa){
         
     }
 }
